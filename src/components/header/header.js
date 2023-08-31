ad9ab73b5907dde58d833f0339fe174401b2c140
gihonhon <agunggihon@gmail.com>
2023-08-31 13:57:36 +0800

    edit header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,6 +8,7 @@ function Header() {
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
+          <li><a href="/contact">Price</a></li>
         </ul>
       </nav>
     </header>
